d613f203f87b4ae8e061258f7b5138e120a3a4a7
Zelito A. Saide <zelitosaide83@gmail.com>
2024-02-10 15:24:53 +0200

    init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            15m Quickstart Blog Tutorial...
           </a>
         </li>
         <li>
